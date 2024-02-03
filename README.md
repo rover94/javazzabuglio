7b4d8d3c09310ae89adbcdca1d6963ca74d79e7f
Matteo Campestrini <matteo.campestrini@transporeon.com>
2024-02-03 22:12:10 +0100

    init project

---
+++ README.md
@@ -0,0 +1,5 @@
+# javazzabuglio
+
+A container for a small set of games or coding activities.
+java
+
---
+++ src/main/java/caribou/personal/Main.java
@@ -0,0 +1,7 @@
+package caribou.personal;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+}
